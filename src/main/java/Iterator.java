5bdbc10f97131f96de8127d7a57b690a9e09a2f2
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2025-04-03 15:20:56 +0000

    Create Iterator interface

---
+++ src/main/java/Iterator.java
@@ -0,0 +1,12 @@
+/**
+ * This interface defines an iterator design pattern.
+ * Runs the iterator examples
+ * @version 1.0
+ * @since 2025-04
+ * @author J. Cihlar
+ */
+
+interface Iterator {
+    public int getNext();
+    public boolean hasMore();
+}
